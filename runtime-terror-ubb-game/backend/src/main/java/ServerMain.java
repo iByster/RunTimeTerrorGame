12b9b32e6ad0e07e5c37efd3a40c51d188c782f6
Irimiciuc Andreea <irimiciuc_anda25@yahoo.com>
2021-11-13 18:50:06 +0200

    Backend Arhitecture

---
+++ runtime-terror-ubb-game/backend/src/main/java/ServerMain.java
@@ -0,0 +1,14 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+
+//@SpringBootApplication
+//@EnableScheduling
+public class ServerMain {
+    public static void main(String[] args) {
+        System.out.println("Success");
+        //SpringApplication.run(ServerMain.class, args);
+    }
+}
---
+++ runtime-terror-ubb-game/backend/src/main/java/controller/controllerImplementation/UserControllerImplementation.java
@@ -0,0 +1,4 @@
+package controller.controllerImplementation;
+
+public class UserControllerImplementation {
+}
