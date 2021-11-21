22d1a6f055f9bd986de2994a1e491c321262f927
Catalina <sapca.catalina@gmail.com>
2021-11-21 22:01:00 +0200

    fixed

--- runtime-terror-ubb-game/backend/src/main/java/server/Main.java
+++ runtime-terror-ubb-game/backend/src/main/java/server/Main.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class ServerMain {
+public class Main {
     public static void main(String[] args) {
-        SpringApplication.run(ServerMain.class, args);
+        SpringApplication.run(Main.class, args);
     }
 }
