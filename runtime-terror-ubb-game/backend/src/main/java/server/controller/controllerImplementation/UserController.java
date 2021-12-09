30a61968fc31b746b3fd6b070f0ac1a27831ea6b
alex.gane <alexandru.gane@evozon.com>
2021-12-09 13:52:28 +0200

    BE changed login to use ResponseEntity

--- runtime-terror-ubb-game/backend/src/main/java/server/controller/controllerImplementation/UserController.java
+++ runtime-terror-ubb-game/backend/src/main/java/server/controller/controllerImplementation/UserController.java
@@ -3,8 +3,11 @@ package server.controller.controllerImplementation;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,20 +18,20 @@ import server.service.serviceImplementation.UserService;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
-
+@CrossOrigin
 @RestController
 public class UserController {
     @Autowired
     private UserService userService;
 
     @PostMapping("/login")
-    public Token login(@RequestBody User user) {
+    public ResponseEntity<?> login(@RequestBody User user) {
         System.out.println("login");
         User fromRepository = userService.find(user.getUsername(), user.getPassword());
         if (fromRepository == null)
-            return new Token("");
+            return new ResponseEntity<String>("User not found", HttpStatus.NOT_FOUND);
         else
-            return new Token(getJWTToken(user.getUsername()));
+            return new ResponseEntity<Token>(new Token(getJWTToken(user.getUsername())), HttpStatus.OK);
     }
 
     @PostMapping("/register")
