3b7576df53e651760f6c91555c0fab7ba14f4325
burnfire <stefanescupetru@yahoo.com>
2021-12-17 02:39:53 +0200

    Am facut ca nivelul sa inceapa cu un countdown.

--- runtime-terror-ubb-game/frontend/src/components/game/Game.tsx
+++ runtime-terror-ubb-game/frontend/src/components/game/Game.tsx
@@ -1,16 +1,41 @@
 import {useParams} from "react-router-dom";
 import "./Game.css";
 import {Player} from "../player/Player";
+import {useEffect, useRef, useState} from "react";
 
 const Game = ()=>
 {
     const {levelId} = useParams();
-    return (
-        <div id={"container"}>
-            <h1>Level {levelId}</h1>
-            <Player player={{life: 100, items: []}}/>
-        </div>
-    );
+    const [timeUntilGameStart, setTimeUntilGameStart] = useState(3);
+    useEffect(()=>
+    {
+        //    Facem un countdown
+        const countdownInterval = setInterval(()=>
+        {
+            if(timeUntilGameStart > 0)
+            {
+                setTimeUntilGameStart(timeUntilGameStart - 1);
+                console.log("Ceva");
+            }
+        }, 1000);
+        return ()=>clearInterval(countdownInterval);
+    }, [timeUntilGameStart]);
+    if(timeUntilGameStart===0)
+    {
+        //Jocul se desfasoara aici
+        return (
+            <div className={"container"}>
+                <h1>Level {levelId}</h1>
+                <Player player={{life: 100, items: []}}/>
+            </div>
+        );
+    }
+    else
+    {
+        return (<div className={"container"} id={"countdown"}>
+            <h1 id={"countdownText"}>{timeUntilGameStart}</h1>
+        </div>);
+    }
 }
 
 export default Game;
