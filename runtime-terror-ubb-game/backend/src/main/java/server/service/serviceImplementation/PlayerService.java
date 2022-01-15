1d4ed6ea05ed012cfbc450c1ccb6c819fe43c28e
Alex Chimpan <62359477+Waryks@users.noreply.github.com>
2022-01-15 23:32:30 +0200

    create player

--- runtime-terror-ubb-game/backend/src/main/java/server/service/serviceImplementation/PlayerService.java
+++ runtime-terror-ubb-game/backend/src/main/java/server/service/serviceImplementation/PlayerService.java
@@ -39,7 +39,8 @@ public class PlayerService {
     public Player updatePlayerScore(String username, Long score) {
         try {
             Player p = playerRepository.getOne(username);
-            p.setScore(score);
+            if(p.getScore() < score)
+                p.setScore(score);
             return playerRepository.save(p);
         } catch (EntityNotFoundException exception) {
             return null;
