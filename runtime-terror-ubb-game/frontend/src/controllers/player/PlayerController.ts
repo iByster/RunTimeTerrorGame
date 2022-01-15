00b7e264f40c0f9acd5ef600a2bfd91c03a84c67
Alex Chimpan <62359477+Waryks@users.noreply.github.com>
2022-01-15 23:19:17 +0200

    Update PlayerController.ts

--- runtime-terror-ubb-game/frontend/src/controllers/player/PlayerController.ts
+++ runtime-terror-ubb-game/frontend/src/controllers/player/PlayerController.ts
@@ -1,10 +1,33 @@
 
 import { json, status } from '../../utils/restUtils';
-
+import Player from './PlayerEntity';
 
 const SERVER_BASE_URL = 'http://127.0.0.1:3001';
 
 export default class PlayerController {
+    createPlayer(player: Player) {
+        const myHeaders = new Headers();
+        myHeaders.append('Accept', 'application/json');
+        myHeaders.append('Content-Type', 'application/json');
+    
+        const antet = {
+          method: 'POST',
+          headers: myHeaders,
+          mode: 'cors' as RequestMode,
+          body: JSON.stringify(Player),
+        };
+    
+        const registerUrl = SERVER_BASE_URL + '/createPlayer';
+    
+        return fetch(registerUrl, antet)
+          .then(status)
+          .then(json)
+          .then((data) => {
+            console.log('Request succeeded with JSON response', data);
+            return data;
+          })
+      }
+
   updatePlayerScore(username: string, score:number) {
     const myHeaders = new Headers();
     myHeaders.append('Accept', 'application/json');
